77749bb1742edf1e37b5dc04fb4b07ab03990989
Leila Torres <leila.torres5800@my.riohondo.edu>
2026-09-01 16:51:26 -0700

    Finish form submission

--- packages/react-frontend/src/Form.jsx
+++ packages/react-frontend/src/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Form() {
+function Form(props) {
   const [person, setPerson] = useState({
     name: "",
     job: "",
@@ -14,6 +14,11 @@ function Form() {
     } else {
       setPerson({ name: value, job: person["job"] });
     }
+  }
+
+  function submitForm() {
+    props.handleSubmit(person);
+    setPerson({ name: "", job: "" });
   }
 
   return (
@@ -35,6 +40,8 @@ function Form() {
         value={person.job}
         onChange={handleChange}
       />
+
+      <input type="button" value="Submit" onClick={submitForm} />
     </form>
   );
 }
