9a0c704bb2179613aa034ddce4248eda9dc2b881
Leila Torres <leila.torres5800@my.riohondo.edu>
2026-09-08 23:45:58 -0700

    Add user ID endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,9 @@ const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
 
@@ -52,6 +55,17 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
